8ba3248c694bf622469a35826d51a7f05f302c33
hukuryo <ryohei.1998.05.09@gmail.com>
2024-12-08 17:32:10 +0900

    add feature checkbox

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { Link } from "expo-router";
-import { View, Text, StyleSheet } from "react-native";
 
-export default function HomeScreen() {
+function MyCheckbox() {
+  const [checked, setChecked] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
+    <Pressable
+      role="checkbox"
+      aria-checked={checked}
+      style={[styles.checkboxBase, checked && styles.checkboxChecked]}
+      onPress={() => setChecked(!checked)}
+    >
+      {checked && <Ionicons name="checkmark" size={24} color="white" />}
+    </Pressable>
+  );
+}
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <Text style={styles.appTitle}>Checkbox Example</Text>
+      <View style={styles.checkboxContainer}>
+        <MyCheckbox />
+        <Text style={styles.checkboxLabel}>{`⬅️ Click!`}</Text>
+      </View>
       <Link href="/details/1">View first user details</Link>
       <Link href="/details/2">View second user details</Link>
     </View>
@@ -12,9 +32,36 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  checkboxBase: {
+    width: 24,
+    height: 24,
     justifyContent: "center",
     alignItems: "center",
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "coral",
+    backgroundColor: "transparent",
+  },
+  checkboxChecked: {
+    backgroundColor: "coral",
+  },
+  appContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  appTitle: {
+    marginVertical: 16,
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  checkboxLabel: {
+    marginLeft: 8,
+    fontWeight: "500",
+    fontSize: 18,
   },
 });
